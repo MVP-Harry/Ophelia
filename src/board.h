4abb6e08ed1424e61712bf558891b4bcf1364d33
Harry Zhou <harry.zhou.xj@outlook.com>
2022-02-21 14:11:04 -0800

    Board

--- src/board.h
+++ src/board.h
@@ -4,6 +4,8 @@
 
 
 #include <bitset>
+#include <cstddef>
+#include <istream>
 
 class Board {
 	// Using bitboard representation
@@ -15,6 +17,19 @@ class Board {
 	bool nextMove; // if nextMove is true, then it's white's turn
 	bool castleWhite; // if white can still castle
 	bool castleBlack; // if black can still castle
+
+	int square[8][8];
+	int epSquare; // En-passant square?
+	int fiftyMove; // move since the last pawn move or capture
+	int material; // total material in centipawn, in white's perspective
+
+	public:
+
+	Board() {
+		// init without any variable
+	}
+
+	Board()
 	
 };
 
